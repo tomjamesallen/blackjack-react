2d1b1dc527577d6b9d6b96a954acb64745943a4c
Tom Allen <tomjamesallenweb@gmail.com>
2016-04-29 21:19:36 +0100

    Linting `scoreComparator.js`.

--- src/js/modules/scoreComparator.js
+++ src/js/modules/scoreComparator.js
@@ -5,38 +5,37 @@
  * blackjack, and which hand has the highest score. Blackjack is not marked if
  * there is a draw.
  */
-var scoreComparator = function(hand1ScoreData, hand2ScoreData) {
-
+function scoreComparator(hand1ScoreData, hand2ScoreData) {
   // Winner placeholder.
-  var winner = {
+  let winner = {
     hand: null,
     blackjack: false
-  };
+  }
 
   // If both hands have blackjacks then it's a drawer.
   if (hand1ScoreData.blackjack && hand2ScoreData.blackjack) {
-    winner.hand = 'draw';
+    winner.hand = 'draw'
   }
 
   // If only one hand has a blackjack then that's the winning hand.
   else if (hand1ScoreData.blackjack) {
-    winner.hand = 1;
-    winner.blackjack = true;
+    winner.hand = 1
+    winner.blackjack = true
   }
   else if (hand2ScoreData.blackjack) {
-    winner.hand = 2;
-    winner.blackjack = true;
+    winner.hand = 2
+    winner.blackjack = true
   }
 
   // If there are no blackjacks in either hand, then we look at the raw scores.
   // If they are equal then it's a drawer, otherwise the higher score wins.
-  else if (hand1ScoreData.score === hand2ScoreData.score) winner.hand = 'draw';
-  else if (hand1ScoreData.score > hand2ScoreData.score) winner.hand = 1;
-  else if (hand1ScoreData.score < hand2ScoreData.score) winner.hand = 2;
+  else if (hand1ScoreData.score === hand2ScoreData.score) winner.hand = 'draw'
+  else if (hand1ScoreData.score > hand2ScoreData.score) winner.hand = 1
+  else if (hand1ScoreData.score < hand2ScoreData.score) winner.hand = 2
 
   // Return the winner. Either 1 or 2, referring to the first and second hand
   // passed to the function.
-  return winner;
-};
+  return winner
+}
 
-module.exports = scoreComparator;
+module.exports = scoreComparator
